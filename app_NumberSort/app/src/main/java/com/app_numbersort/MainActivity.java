4a77241ba97b1dc1662307e9f4f60634fa0d2b1c
Alexandre de Souza Eustaquio <alexeus.souza@gmail.com>
2022-10-14 09:23:06 -0300

    App NumberSort

--- app_NumberSort/app/src/main/java/com/app_numbersort/MainActivity.java
+++ app_NumberSort/app/src/main/java/com/app_numbersort/MainActivity.java
@@ -3,6 +3,11 @@ package com.app_numbersort;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.WindowInsetsAnimation;
+import android.widget.TextView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void alterarTexto(View view){
+
+        TextView Nome = findViewById(R.id.txtNome);
+        TextView Multiline = findViewById(R.id.txtMultiLine);
+        TextView numeroSorteado = findViewById(R.id.txtValorSorteado);
+        Multiline.setText(Nome.getText());
+        int x = new Random().nextInt(10); // Gera numeros aleatorios de 0 ate 9
+       numeroSorteado.setText("" + x);
+    }
+
+    public void limparTexto(View view){
+
+        TextView txtNome = findViewById(R.id.txtNome);
+        TextView Multiline = findViewById(R.id.txtMultiLine);
+        TextView numeroSorteado = findViewById(R.id.txtValorSorteado);
+
+        Multiline.setText("");
+        txtNome.setText("");
+        numeroSorteado.setText("");
     }
 }
