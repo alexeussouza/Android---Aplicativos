f338305118432eb5086befbe4fe50f8b2c080197
Alexandre de Souza Eustaquio <alexeus.souza@gmail.com>
2022-10-27 09:31:07 -0300

    update

--- M11_A03_RecyclerView/app/src/main/java/com/example/recyclerview/activity/activity/adapter/Adapter.java
+++ M11_A03_RecyclerView/app/src/main/java/com/example/recyclerview/activity/activity/adapter/Adapter.java
@@ -2,6 +2,7 @@ package com.example.recyclerview.activity.activity.adapter;
 
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
@@ -25,6 +26,10 @@ public class Adapter extends RecyclerView.Adapter<Adapter.MyViewHolder> {
     }
 
     public class  MyViewHolder extends  RecyclerView.ViewHolder{
+
+        TextView titulo;
+        TextView ano;
+        TextView genero;
 
         public MyViewHolder(@NonNull View itemView) {
             super(itemView);
